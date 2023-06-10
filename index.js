5d087c6b6565bedef6071700a393369e4f0da103
juber2023 <juber.eco@gmail.com>
2023-06-10 11:04:51 +0600

    manage classes put done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.8eid0qf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,19 +33,6 @@ async function run() {
      const classesCollection = client.db("SummerCampDB").collection("classes");
      const instructorsCollection = client.db("SummerCampDB").collection("instructors");
 
-     app.post('/users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUser = await usersCollection.findOne(query);
-  
-        if (existingUser) {
-          return res.send({ message: 'user already exists' })
-        }
-  
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
-      });
-
       app.get("/users", async (req, res) => {
         const cursor = usersCollection.find();
         const result = await cursor.toArray();
@@ -63,6 +50,19 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
       });
+
+    
+      app.put('/classes/:id', async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)}
+        const options={upsert:true}
+        const cls=req.body
+        const updatedClass={
+          $set:{type:cls.type}
+        }
+        const result= await classesCollection.updateOne(query,updatedClass,options)
+        res.send(result)
+      })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
